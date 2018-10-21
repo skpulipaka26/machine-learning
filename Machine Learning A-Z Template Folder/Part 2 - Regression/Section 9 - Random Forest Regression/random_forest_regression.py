cf52a51020b8e5d0f570c20589d442ba6f3bd047
skpulipaka26 <31779125+skpulipaka26@users.noreply.github.com>
2018-10-21 11:47:58 -0700

    random forest regression - finished with prediction

--- Machine Learning A-Z Template Folder/Part 2 - Regression/Section 9 - Random Forest Regression/random_forest_regression.py
+++ Machine Learning A-Z Template Folder/Part 2 - Regression/Section 9 - Random Forest Regression/random_forest_regression.py
@@ -1,6 +1,7 @@
-# Decision Tree Regression
+# Random Forest Regression
 
 # Importing the libraries
+from sklearn.ensemble import RandomForestRegressor
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -22,20 +23,19 @@ X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
 
-# Fitting Decision Tree Regression to the dataset
-from sklearn.tree import DecisionTreeRegressor
-regressor = DecisionTreeRegressor(random_state = 0)
+# Fitting Random Forest Regression to the dataset
+regressor = RandomForestRegressor(n_estimators=10, random_state=0)
 regressor.fit(X, y)
 
 # Predicting a new result
 y_pred = regressor.predict(6.5)
 
-# Visualising the Decision Tree Regression results (higher resolution)
+# Visualising the Random Forest Regression results (higher resolution)
 X_grid = np.arange(min(X), max(X), 0.01)
 X_grid = X_grid.reshape((len(X_grid), 1))
-plt.scatter(X, y, color = 'red')
-plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
-plt.title('Truth or Bluff (Decision Tree Regression)')
+plt.scatter(X, y, color='red')
+plt.plot(X_grid, regressor.predict(X_grid), color='blue')
+plt.title('Truth or Bluff (Random Forest Regression)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
